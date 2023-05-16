c3de8bfb2d82fa5f481a642d41700acb9c604cfe
ammar ikhlass <ikhlassammar7@gmail.com>
2023-05-16 09:29:34 +0100

    firstappredux

--- src/App.js
+++ src/App.js
@@ -1,23 +1,16 @@
-import Addtasks from './component/task/Addtasks';
-import Tasklist from './component/task/Tasklist';
-import Edittasks from './component/task/Edittasks';
-
-
+import Addtasks from "./component/task/Addtasks";
+import Tasklist from "./component/task/Tasklist";
+import Edittasks from "./component/task/Edittasks";
 
 function App() {
   return (
-   
-      
-          <div >
-            <h1 > TODO LIST </h1>
-            <Addtasks />
-            <Tasklist />
-            <Edittasks/>
-          </div>
-       
-    );
-    
-
+    <div className="app">
+      <h1> TODO LIST </h1>
+      <Addtasks />
+      <Tasklist />
+      <Edittasks />
+    </div>
+  );
 }
 
 export default App;
